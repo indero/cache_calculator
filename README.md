dde4fd6e14bf27b6aac8c669eac9a08c4c6e87db
Daniel Steudler <daniel.steudler@gmx.ch>
2018-02-02 20:48:14 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,111 @@
-# cache_calculator
+# Cache Calculator
+
+This is a little ruby script I've written to better understand
+Fully Associative, Direct Mapped and K-Way Set-Associative-Caches
+
+For education purpose
+
+```ruby
+#Fully Associative Cache:
+# 4KB Cache, 22 bit address space and 64 Byte per memory block
+fac = FullyAssociativeCache.new(2**12, 22, 2**6)
+puts fac.print
+# 4-Way Set-Associative-Cache:
+# 4 Way, 64KB Cache, 32 bit address space and 32 byte per memory Block
+kway = KWay.new(4, 2**16, 32, 2**5)
+puts kway.print
+# DirectMapped Cache:
+# 64KB Cache, 32 bit address space and 32 byte per memory Block
+directmapped = DirectMapped.new(2**16, 32, 2**5)
+puts directmapped.print
+```
+Will output:
+```
+#############################
+Fully Associative Cache
+The cache is 4096 bytes (2^12)
+We have a 22 bit address space
+And one block is 64 bytes (2^6)
+
+## Total blocks in cache ##
+Cache size / block size = number of blocks
+4096 / 64 = 64 blocks
+= 2^6 blocks
+
+## Tag and Offset ##
+### Offset ###
+log2(block size) = offset
+log2(64) = 6 bit
+### Tag ###
+Address bits - offset = tag
+22 - 6 = 22 bit tag
+
+### Tag Overhead ###
+Bits per tag * total number of blocks = overhead
+22 * 64 = 1408 bit = 176 byte
+
+#############################
+4-Way Set-Associative
+The cache is 65536 bytes (2^16)
+We have a 32 bit address space
+And one block is 32 bytes (2^5)
+
+## Total blocks in cache ##
+Cache size / block size = number of blocks
+65536 / 32 = 2048 blocks
+= 2^11 blocks
+
+## Blocks per Way (= number of sets) ##
+Total number of blocks / number of ways =
+2048/4 = 512
+
+### Size per Way ###
+Blocks per way * Block size = bytes per way
+512 * 32 = 16384 Byte
+or
+cache size / number of ways = bytes per way
+65536 / 4 = 16384 Byte = 2^9 Byte
+
+## Offset, Set and Tag ##
+### Offset ###
+log2(block size) = offset
+log2(32) = 5 bit
+### Set ###
+log2(blocks per way) = set
+log2(512) = 9 bit
+### Tag ###
+Address space - set - offset = tag
+32 - 9 - 5 = 18 bit tag
+
+### Tag Overhead ###
+Bits per tag * total number of blocks = overhead
+18 * 2048 = 65536 bit = 8192 byte
+
+#############################
+Direct Mapped Cache
+The cache is 65536 bytes (2^16)
+We have a 32 bit address space
+And one block is 32 bytes (2^5)
+
+## Total blocks in cache ##
+Cache size / block size = number of blocks
+65536 / 32 = 2048 blocks
+= 2^11 blocks
+
+Remember: A Direct Mapped Cache is a k-Way Set-Associative-Cache with only one way
+
+## Offset, Set and Tag ##
+### Offset ###
+log2(block size) = offset
+log2(32) = 5 bit
+### Set ###
+log2(blocks per way) = set
+log2(2048) = 11 bit
+### Tag ###
+Address space - set - offset = tag
+32 - 11 - 5 = 16 bit tag
+
+### Tag Overhead ###
+Bits per tag * total number of blocks = overhead
+16 * 2048 = 65536 bit = 8192 byte
+```
---
+++ cache.rb
@@ -0,0 +1,191 @@
+#!/bin/env ruby
+#
+#The word Block translates to Zeile in German
+class FullyAssociativeCache
+  def initialize(size, address, block_size)
+    @cache_size = size
+    @address_bits = address
+    @block_size = block_size
+    @number_of_blocks = calculate_number_of_blocks
+    @offset = calculate_offset
+    @tag = @address_bits
+    @overhead = calculate_tag_overhead
+  end
+
+  def calculate_number_of_blocks
+    return @cache_size/@block_size
+  end
+
+  def print_info
+    puts "The cache is #{@cache_size} bytes (2^#{Math.log(@cache_size,2).to_i})"
+    puts "We have a #{@address_bits} bit address space"
+    puts "And one block is #{@block_size} bytes (2^#{Math.log(@block_size,2).to_i})"
+    puts
+  end
+  def print
+    puts "#############################"
+    puts "Fully Associative Cache"
+    print_info
+    print_number_of_blocks
+    puts "## Tag and Offset ##"
+    print_offset
+    print_tag
+    print_tag_overhead
+  end
+
+  def print_number_of_blocks
+    puts "## Total blocks in cache ##"
+    puts "Cache size / block size = number of blocks"
+    puts "#{@cache_size} / #{@block_size} = #{@number_of_blocks} blocks"
+    puts "= 2^#{Math.log(@number_of_blocks,2).to_i} blocks"
+    puts
+  end
+
+  def print_tag
+    puts "### Tag ###"
+    puts "Address bits - offset = tag"
+    puts "#{@address_bits} - #{@offset} = #{@tag} bit tag"
+    puts
+  end
+
+  def calculate_tag_overhead
+    return @tag * @number_of_blocks
+  end
+
+  def print_tag_overhead
+    puts "### Tag Overhead ###"
+    puts "Bits per tag * total number of blocks = overhead"
+    puts "#{@tag} * #{@number_of_blocks} = #{@overhead} bit = #{@overhead/8} byte"
+  end
+
+  def calculate_offset
+    return Math.log(@block_size,2).to_i
+  end
+
+  def calculate_tag
+    return @address_bits - @offset
+  end
+
+  def print_offset
+    puts "### Offset ###"
+    puts "log2(block size) = offset"
+    puts "log2(#{@block_size}) = #{@offset} bit"
+  end
+
+end
+
+class KWay < FullyAssociativeCache
+  def initialize(ways, size, address, block_size)
+    super(size, address, block_size)
+    @number_of_ways = ways
+    @blocks_per_way = calculate_blocks_per_way
+    @size_per_way_variant_one = calculate_size_per_way_variant_one
+    @size_per_way_variant_two = calculate_size_per_way_variant_two
+    @offset = calculate_offset
+    @set = calculate_set
+    @tag = calculate_tag
+  end
+
+  def calculate_blocks_per_way
+    return @number_of_blocks/@number_of_ways
+  end
+
+  def calculate_size_per_way_variant_one
+    return @blocks_per_way * @block_size
+  end
+
+  def calculate_size_per_way_variant_two
+    return @cache_size / @number_of_ways
+  end
+
+  def print_wayinfo
+    puts "## Blocks per Way (= number of sets) ##"
+    puts "Total number of blocks / number of ways ="
+    puts "#{@number_of_blocks}/#{@number_of_ways} = #{@blocks_per_way}"
+    puts
+    puts "### Size per Way ###"
+    puts "Blocks per way * Block size = bytes per way"
+    puts "#{@blocks_per_way} * #{@block_size} = #{@size_per_way_variant_one} Byte"
+    puts "or"
+    puts "cache size / number of ways = bytes per way"
+    puts "#{@cache_size} / #{@number_of_ways} = #{@size_per_way_variant_two} Byte = 2^#{Math.log(@size_per_way_variant_two).to_i} Byte"
+    puts
+  end
+
+  def print
+    puts "#############################"
+    puts "#{@number_of_ways}-Way Set-Associative"
+    print_info
+    print_number_of_blocks
+    print_wayinfo
+    puts "## Offset, Set and Tag ##"
+    print_offset
+    print_set
+    print_tag
+    print_tag_overhead
+  end
+
+  def calculate_offset
+    return Math.log(@block_size,2).to_i
+  end
+
+  def print_offset
+    puts "### Offset ###"
+    puts "log2(block size) = offset"
+    puts "log2(#{@block_size}) = #{@offset} bit"
+  end
+
+  def calculate_set
+    return Math.log(@blocks_per_way,2).to_i
+  end
+
+  def print_set
+    puts "### Set ###"
+    puts "log2(blocks per way) = set"
+    puts "log2(#{@blocks_per_way}) = #{@set} bit"
+  end
+
+  def calculate_tag
+    return @address_bits - @set - @offset
+  end
+
+  def print_tag
+    puts "### Tag ###"
+    puts "Address space - set - offset = tag"
+    puts "#{@address_bits} - #{@set} - #{@offset} = #{@tag} bit tag"
+    puts
+  end
+
+end
+class DirectMapped < KWay
+  def initialize(size, address, block_size)
+    super(1, size, address, block_size)
+  end
+  def print
+    puts "#############################"
+    puts "Direct Mapped Cache"
+    print_info
+    print_number_of_blocks
+    puts "Remember: A Direct Mapped Cache is a k-Way Set-Associative-Cache with only one way"
+    puts
+    puts "## Offset, Set and Tag ##"
+    print_offset
+    print_set
+    print_tag
+    print_tag_overhead
+  end
+
+end
+
+#Fully Associative Cache:
+# 4KB Cache, 22 bit address space and 64 Byte per memory block
+fac = FullyAssociativeCache.new(2**12, 22, 2**6)
+puts fac.print
+# 4-Way Set-Associative-Cache:
+# 4 Way, 64KB Cache, 32 bit address space and 32 byte per memory Block
+kway = KWay.new(4, 2**16, 32, 2**5)
+puts kway.print
+# DirectMapped Cache:
+# 64KB Cache, 32 bit address space and 32 byte per memory Block
+directmapped = DirectMapped.new(2**16, 32, 2**5)
+puts directmapped.print
